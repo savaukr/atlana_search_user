e943759d113aedb078b2eccff0696c2f0dd2d08d
SlavaPachevsky <savaukr17101980@gmail.com>
2022-08-28 16:24:41 +0300

    users table

--- src/components/users/users.tsx
+++ src/components/users/users.tsx
@@ -1,17 +1,30 @@
-import React, { useState, useCallback, useEffect } from "react";
+import React, { useState, useCallback, useEffect, useMemo } from "react";
 import { useNavigate } from "react-router-dom";
 
 import { TUser } from "../user.type";
 
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
 import TextField from "@mui/material/TextField";
 import { debounce } from "../../helpers/debounce";
 import iconAttention from "../../images/Icon_attention.png";
 
 import styles from "./users.module.scss";
+import { isBrowser } from "@emotion/utils";
 
 // onClick={() => {
 //     navigate(`/${user.id}`);
 //   }}
+
+const columns = [
+  { name: "Avatar", width: "25%" },
+  { name: "Name", width: "45%" },
+  { name: "Link", width: "30%" },
+];
 
 export default function Users() {
   const navigate = useNavigate();
@@ -21,21 +34,10 @@ export default function Users() {
 
   useEffect(() => {
     const usersLocalStorage = localStorage.getItem("users");
+    console.log("usersLocalStorage:", usersLocalStorage);
     if (usersLocalStorage) setUsers(JSON.parse(usersLocalStorage));
+    else localStorage.setItem("users", JSON.stringify(users));
   }, []);
-
-  // fetch(`https://api.github.com/users/${user.name}`))
-
-  // try {
-  //   let response = await fetch(
-  //     "https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5"
-  //   );
-  //   const json = await response.json();
-  //   setExchRate([json[0], json[1]]);
-  // } catch (err) {
-  //   err.message = "Can not get exchange rate, try latter";
-  //   throw err;
-  // }
 
   const getSearchUsers = useCallback(async (userName: string) => {
     let timerId;
@@ -55,6 +57,29 @@ export default function Users() {
   }, []);
 
   const debounceGetSearchUser = useCallback(debounce(getSearchUsers, 1000), []);
+
+  const rows = useMemo(
+    () =>
+      users.reduce<
+        {
+          id: number;
+          img: string;
+          name: string | null;
+          login: string;
+          link: string;
+        }[]
+      >((acc, user) => {
+        acc.push({
+          id: user.id,
+          img: user.avatar_url,
+          name: user.name,
+          login: user.login,
+          link: user.html_url,
+        });
+        return acc;
+      }, []),
+    [users]
+  );
 
   return (
     <div className={styles.wrapper}>
@@ -82,6 +107,50 @@ export default function Users() {
           debounceGetSearchUser(event.target.value);
         }}
       />
+      <div className={styles.tableUsers}>
+        <TableContainer sx={{ maxHeight: 440 }}>
+          <Table stickyHeader aria-label="sticky table">
+            <TableHead>
+              <TableRow>
+                {columns.map((column) => (
+                  <TableCell
+                    key={column.name}
+                    align={"left"}
+                    style={{ width: column.width }}
+                  >
+                    {column.name}
+                  </TableCell>
+                ))}
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {rows.map((row) => {
+                return (
+                  <TableRow hover tabIndex={-1} key={row.id}>
+                    <TableCell
+                      onClick={() => navigate(`/${row.id}`)}
+                      className={styles.hover}
+                    >
+                      <img src={row.img} width="40px" height="40px" />
+                    </TableCell>
+                    <TableCell
+                      onClick={() => navigate(`/${row.id}`)}
+                      className={styles.hover}
+                    >
+                      {row.name || row.login}
+                    </TableCell>
+                    <TableCell>
+                      <a href={row.link} target="blank" className={styles.link}>
+                        link to repo
+                      </a>
+                    </TableCell>
+                  </TableRow>
+                );
+              })}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </div>
     </div>
   );
 }
